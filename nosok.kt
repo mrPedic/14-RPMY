dabde86fe42f290da33dd562249a552c213049ba
mrPedic <vladvasilo240@gmail.com>
2025-06-14 14:49:28 +0300

    1st.kt

--- nosok.kt
+++ nosok.kt
@@ -3,17 +3,17 @@ fun main() {
     val password: String
     val confirmPassword: String
 
-    print("Введите логин : ")
+    print("Введите логин (от 8 до 20 символов) : ")
     readln().also { login = it }
 
-    print("Введите пароль : ")
+    print("Введите пароль (от 8 до 20 символов): ")
     readln().also { password = it }
 
     print("Повторите пароль : ")
     readln().also { confirmPassword = it }
-    
+
     if (isCorrectInput(login, password, confirmPassword))
-        println("Введенные данные соответствуют требованиям!!!") 
+        println("Введенные данные соответствуют требованиям!!!")
     else
         println("Введенные данные не соответствуют требованиям ")
 }
@@ -22,18 +22,24 @@ fun isCorrectInput (
      login: String,
      password: String,
      confirmPassword: String
-): Boolean {
-    if (password != confirmPassword)
+): Boolean
+{
+    if (password != confirmPassword) {
+        println("Пароли отличаются")
         return false
-    if (password.length < 8 || password.length > 20)
+    }
+    if (password.length < 8 || password.length > 20) {
+        println("Не подходящая длина пароля")
         return false
-    if (login.length < 8 || login.length > 20)
+    }
+    if (login.length < 8 || login.length > 20) {
+        println("Неподходящая длина логина")
         return false
+    }
 
     if (login.windowed(3).any{ it in password }){
         println("В пароле есть повторяющиеся символы ")
         return false
     }
-
     return true
 }
